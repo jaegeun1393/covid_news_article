c7d566d896bb46167d3d693df200b856543bfb1b
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2020-04-20 16:27:00 -0700

    ver.2 | netlify

--- crawling.py
+++ crawling.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('home.html')
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-        if request.method == 'POST':
-            result = request.form
-            return render_template("index_result.html", result=result)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
